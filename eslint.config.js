1e1db25030984144e47fdaac881872b071feb2e3
Evgeny_Stepanov <step.evgeny@gmail.com>
2026-03-04 16:43:35 +0300

    chore: update eslint config with style rules

--- eslint.config.js
+++ eslint.config.js
@@ -7,16 +7,31 @@ export default [
   {
     files: ['**/*.js'],
     languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
     rules: {
-      'no-unused-vars': ['error', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_' 
-      }],
+      // Ошибки
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': 'off',
+
+      // Стиль (добавляем)
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
     },
   },
 ];
